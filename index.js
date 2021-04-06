5edff7afcb601762ea6f505c96e9c16e706ecccc
Laura Nielsen <nielsen.j.laura@gmail.com>
2021-04-06 14:21:26 -0400

    commit tree ca16fee977639ce571f6bd0f9ae83b1a59db0ca0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (someValue) {
+returns (someValue);
+}
+
+function distanceFromHqInFeet (someValue) {
+    distanceFromHqInBlocks (someValue)
+}
